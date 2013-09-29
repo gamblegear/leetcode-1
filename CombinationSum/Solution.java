011c7ef6a51fda7ff27207eedad9c8ec605b3a70
Zeiga Zhao <zeiga.zhao@gmail.com>
2013-09-29 11:13:49 -0700

    Solved combination Sum problem.

---
+++ CombinationSum/Solution.java
@@ -0,0 +1,59 @@
+// Given a set of candidate numbers (C) and a target number (T), 
+// find all unique combinations in C where the candidate numbers 
+// sums to T.
+
+// The same repeated number may be chosen from C unlimited number
+// of times.
+// Note:
+// All numbers (including target) will be positive integers.
+// Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+// The solution set must not contain duplicate combinations.
+// For example, given candidate set 2,3,6,7 and target 7, 
+// A solution set is: 
+// [7] 
+// [2, 2, 3] 
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		int[] candidates = { 2, 3, 6, 7 };
+		int target = 7;
+		ArrayList<ArrayList<Integer>> res = combinationSum(candidates, target);
+
+		for (ArrayList<Integer> combo : res) {
+			for (Integer i : combo) {
+				System.out.print(i + ",");
+			}
+			System.out.println();
+		}
+	}
+
+	public static ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
+		Arrays.sort(candidates);
+		return combinationSumSorted(candidates, target);
+	}
+
+    public static ArrayList<ArrayList<Integer>> combinationSumSorted(int[] candidates, int target) {
+		HashSet<ArrayList<Integer>> res = new HashSet<ArrayList<Integer>>();
+		if (target < candidates[0]) {
+			return new ArrayList<ArrayList<Integer>>(res); // empty
+		}
+		for (int c : candidates) {
+			if (target - c == 0) {
+				ArrayList<Integer> s = new ArrayList<Integer>();
+				s.add(c);
+				res.add(s);
+			}
+			else if (target - c > 0) {
+				ArrayList<ArrayList<Integer>> prev = combinationSumSorted(candidates, target - c);
+				for (ArrayList<Integer> combo : prev) {
+					ArrayList<Integer> cc = new ArrayList<Integer>(combo);
+					cc.add(c);
+					Collections.sort(cc);
+					res.add(cc);							
+				}						
+			}
+		}
+		return new ArrayList<ArrayList<Integer>>(res);
+	}
+}
